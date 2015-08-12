4ff23a5844ddcd1862b8a0d93ea6f058766c2b12
Ken Galliher <natas333x2@gmail.com>
2015-08-12 12:46:13 -0700

    Formatting updates

--- kg-sql/sql-server/feature-classes-in-feature-dataset.sql
+++ kg-sql/sql-server/feature-classes-in-feature-dataset.sql
@@ -1,10 +1,11 @@
+
 /* 
-    Ken Galliher - 08/12/15
-	Select feature classes in a feature dataset.
+Ken Galliher - 08/12/15
+Select feature classes in a feature dataset.
 */
 SELECT GDB_ITEMS.UUID AS "Dataset ID",
        substr(GDB_ITEMS.NAME,1,(INSTR(GDB_ITEMS.NAME,'.',1)-1)) AS "Owner"
-FROM GDB_ITEMTYPES,
-  GDB_ITEMS
+FROM GDB_ITEMTYPES, GDB_ITEMS
 WHERE GDB_ITEMTYPES.UUID = GDB_ITEMS.TYPE
 AND GDB_ITEMTYPES.NAME = 'Feature Dataset'
+
