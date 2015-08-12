cf05aa0710f8e23f668e9840e0146ef13060f5eb
Ken Galliher <natas333x2@gmail.com>
2015-08-12 12:35:59 -0700

    Added some breathing room at the bottom

--- kg-sql/postgres/find-active-edit-sessions.sql
+++ kg-sql/postgres/find-active-edit-sessions.sql
@@ -11,3 +11,4 @@ column state_id format a10
 SELECT nodename as username, server_id as spid, start_time, c.state_id, direct_connect
 FROM sde.process_information b, sde.state_locks a, sde.states c
 WHERE b.sde_id = a.sde_id AND a.state_id = c.state_id;
+
