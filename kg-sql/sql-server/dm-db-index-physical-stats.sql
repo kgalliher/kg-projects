c406c9ab18e3fe0449c9c116e7ac23bd4c1bb11b
Ken Galliher <natas333x2@gmail.com>
2015-08-12 12:44:18 -0700

    Formatting

--- kg-sql/sql-server/dm-db-index-physical-stats.sql
+++ kg-sql/sql-server/dm-db-index-physical-stats.sql
@@ -1,11 +1,13 @@
 /*
-	Ken Galliher - 08/12/15
-	Discover index fragmentation for specific tables.
+Ken Galliher - 08/12/15
+Discover index fragmentation for specific tables.
 */
 SELECT  a.index_id, 
-		name, 
-		avg_fragmentation_in_percent
+	name, 
+	avg_fragmentation_in_percent
 FROM sys.dm_db_index_physical_stats (DB_ID(N'BBOXTEST'), OBJECT_ID(N'SDE.ROADS'), NULL, NULL, NULL) AS a
     JOIN sys.indexes AS b 
 	ON a.object_id = b.object_id 
 	AND a.index_id = b.index_id; 
+	
+	
