ab1bd10f9afa7779ebc4f0814c0375e9a881ecdc
Ken <natas333x2@gmail.com>
2015-12-22 19:53:39 -0800

    Added .net folder and some cs files
    Added kill-process-by-timespan

---
+++ kg-dotnet/kill-process-by-timespan/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+
+
+namespace RobloxKill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /* https://msdn.microsoft.com/en-us/library/system.speech.synthesis.speechsynthesizer */
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SpeakAsync("Hello there. You have been playing row blocks way too long.  Time to study.");
+
+        }
+    }
+}
---
+++ kg-dotnet/kill-process-by-timespan/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Diagnostics;
+
+namespace RobloxKill
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            Process[] localByName = Process.GetProcessesByName("RobloxPlayerBeta");
+            if(localByName.Length > 0)
+            {
+                foreach (var prc in localByName)
+                {
+                    DateTime strtTime = prc.StartTime;
+
+                    if (now.TimeOfDay.Hours - strtTime.TimeOfDay.Hours > 2)
+                    {
+                        prc.Kill();
+                        Application.EnableVisualStyles();
+                        Application.SetCompatibleTextRenderingDefault(false);
+                        Application.Run(new Form1());
+                    }
+                }
+            }         
+        }
+    }
+}
