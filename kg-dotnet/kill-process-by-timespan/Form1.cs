18cc4a3058a40070124fce5506cf17934326964f
Ken Galliher <natas333x2@gmail.com>
2015-12-22 20:08:43 -0800

    usings - clean up

--- kg-dotnet/kill-process-by-timespan/Form1.cs
+++ kg-dotnet/kill-process-by-timespan/Form1.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 using System.Windows.Forms;
 using System.Speech.Synthesis;
-using System.Threading;
-
 
 namespace RobloxKill
 {
@@ -26,7 +17,6 @@ namespace RobloxKill
             /* https://msdn.microsoft.com/en-us/library/system.speech.synthesis.speechsynthesizer */
             SpeechSynthesizer synth = new SpeechSynthesizer();
             synth.SpeakAsync("Hello there. You have been playing row blocks way too long.  Time to study.");
-
         }
     }
 }
