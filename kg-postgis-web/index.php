8af65b8d9bfc9357ea5615122b1dbdd2ef64f8c5
Ken Galliher <natas333x2@gmail.com>
2016-01-11 10:43:50 -0800

    Indentation

--- kg-postgis-web/index.php
+++ kg-postgis-web/index.php
@@ -44,69 +44,68 @@ function popup(mylink, windowname) { if (! window.focus)return true; var href; i
             <p>Load GeoJSON directly from Postgres into the map as a layer. <a href="https://github.com/esri/Terraformer">Terraformer</a> is used to convert GeoJSON to ArcGIS JSON.</p>
             <div class="form text-left">
                 <div class="form-group">
-					
                     <label class="control-label">Available Layers</label>
-					<select class="form-control" id="selGeoJson">
-						<option></option>
-						<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option value={$url}>{$layer}</option>"; } ?>
-					</select>
+			<select class="form-control" id="selGeoJson">
+				<option></option>
+				<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option value={$url}>{$layer}</option>"; } ?>
+			</select>
                 </div>
                 <div class="form-group">
                     <label class="control-label">Your Data <a target="_blank" href="https://developers.arcgis.com/javascript/jshelp/ags_proxy.html">(URL/File/CORS Server)</a></label>
                     <div class="input-group">
                       <input type="text" class="form-control" id="geoJsonUrl" value="geojson-files/airports.geojson" placeholder="geojson-files/airports.geojson">
                       <span class="input-group-btn">
-							<button id="btnAdd" class="btn btn-success" type="button">Load</button>
+			<button id="btnAdd" class="btn btn-success" type="button">Load</button>
                       </span>
                     </div>
-				</div>
-				<div class="form-group">
-					<label class="control-label">View Layer Extent</label>
-					<div class="input-group">
-						<select class="form-control" id="selGeoJsonHighlight">
-							<option></option>
-							<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option value='{$url}&get_bbox=Y'>{$layer}</option>"; } ?>
-						</select>
-					</div>
+		</div>
+		<div class="form-group">
+			<label class="control-label">View Layer Extent</label>
+			<div class="input-group">
+				<select class="form-control" id="selGeoJsonHighlight">
+					<option></option>
+					<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option value='{$url}&get_bbox=Y'>{$layer}</option>"; } ?>
+				</select>
+			</div>
                 </div>
-				<br />
-				<div class="form-group">
-					<label class="control-label">Buffer a point layer</label>
-					<div class="input-group">
-						<select class="form-control" id="selGeoJsonBuff">
-							<option></option>
-							<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option id='buffItem' name='{$layer}' value={$url}>{$layer}</option>"; } ?>
-						</select>
-						<label class="control-label">Enter a buffer distance:</label>
-						<div class="input-group">
-						<input type="text" class="form-control" id="buffDist" value="0.05" placeholder="0.05">
-						<span class="input-group-btn">
-							<button id="btnBuff" class="btn btn-success" type="button">Buffer</button>
-					  </span>
-						</div>
-					</div>
+	<br />
+	<div class="form-group">
+		<label class="control-label">Buffer a point layer</label>
+		<div class="input-group">
+			<select class="form-control" id="selGeoJsonBuff">
+				<option></option>
+				<?php foreach(PGDatabase::layer_url() as $layer => $url){ echo "<option id='buffItem' name='{$layer}' value={$url}>{$layer}</option>"; } ?>
+			</select>
+			<label class="control-label">Enter a buffer distance:</label>
+			<div class="input-group">
+			<input type="text" class="form-control" id="buffDist" value="0.05" placeholder="0.05">
+			<span class="input-group-btn">
+				<button id="btnBuff" class="btn btn-success" type="button">Buffer</button>
+		  </span>
+			</div>
+		</div>
+	</div>
+		<br />
+		<div class="form-group">
+			<label class="control-label">Intersection</label>
+			<div class="input-group">
+				<select class="form-control" id="selGeoJsonIntersect1">
+					<option></option>
+					<?php foreach(PGDatabase::get_geom_table_info() as $table => $shape){ echo  "<option id='input2' value='{$table},{$shape}'>{$table}</option>";} ?>
+				</select>
+				<label class="control-label">Enter a layer to intersect:</label>
+				<div class="input-group">
+				<select class="form-control" id="selGeoJsonIntersect2">
+					<option></option>
+					<?php foreach(PGDatabase::get_geom_table_info() as $table => $shape){ echo  "<option id='input2' value='{$table},{$shape}'>{$table}</option>";} ?>
+				</select>
+				<span class="input-group-btn">
+					<button id="btnIntersect" class="btn btn-success" type="button">Intersect</button>
+				</span>
 				</div>
-					<br />
-					<div class="form-group">
-						<label class="control-label">Intersection</label>
-						<div class="input-group">
-							<select class="form-control" id="selGeoJsonIntersect1">
-								<option></option>
-								<?php foreach(PGDatabase::get_geom_table_info() as $table => $shape){ echo  "<option id='input2' value='{$table},{$shape}'>{$table}</option>";} ?>
-							</select>
-							<label class="control-label">Enter a layer to intersect:</label>
-							<div class="input-group">
-							<select class="form-control" id="selGeoJsonIntersect2">
-								<option></option>
-								<?php foreach(PGDatabase::get_geom_table_info() as $table => $shape){ echo  "<option id='input2' value='{$table},{$shape}'>{$table}</option>";} ?>
-							</select>
-							<span class="input-group-btn">
-								<button id="btnIntersect" class="btn btn-success" type="button">Intersect</button>
-							</span>
-							</div>
-							<br />
-						</div>
-					</div>
+				<br />
+			</div>
+		</div>
                 <div class="form-group text-center">
                     <button id="btnRemove" class="btn btn-default btn-block">Clear All</button>
                 </div>
