0b7c57df2ae5451a2e02c0c6f6e32cdfa514a84f
natas333x2 <natas333x2@gmail.com>
2016-04-05 15:59:49 -0700

    Added SQL Server Spatial cursor
    Lat/Long to Geography points

---
+++ kg-sql/sql-server/lat-long-to-geography.sql
@@ -0,0 +1,19 @@
+DECLARE pt_curse CURSOR FOR
+	SELECT latCol, lonCol, id FROM TableWithLatLong
+
+DECLARE @latVar numeric(38,8), @lonVar numeric(38,8), @id int;
+
+OPEN pt_Curse;
+FETCH NEXT FROM pt_curse INTO @latVar, @lonVar, @id
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	--PRINT CAST(@latVar as VARCHAR) + ' == ' + CAST(@lonVar as VARCHAR);
+	UPDATE TableWithLatLong SET shape = geography::Point(@latVar, @lonVar, 4326) WHERE id = @id;
+	PRINT 'DONE';
+	
+	FETCH NEXT FROM pt_curse INTO @latVar, @lonVar, @id
+END;
+
+CLOSE pt_curse;
+DEALLOCATE pt_curse;
