d7aa2fea52fcade531504314a23f18b0497f07b8
natas333x2 <natas333x2@gmail.com>
2017-11-15 14:01:01 -0800

    Remove History.db

--- BrowserHistory/MainWindow.xaml.cs
+++ BrowserHistory/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BrowserHistory
 {
@@ -63,6 +51,7 @@ namespace BrowserHistory
             string tmpPath = System.IO.Path.GetTempPath();
             string copyname = @"History.db";
             string historyCopyPath = System.IO.Path.Combine(tmpPath, copyname);
+            LabelMessenger(historyCopyPath);
             if (File.Exists(filename))
             {
                 try
