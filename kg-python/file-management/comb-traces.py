f364ce69c3a3563207837f5bb6a4b366027d240a
Ken Galliher <kgalliher@esri.com>
2021-08-29 19:59:51 -0700

    Use context mgr in file search

--- kg-python/file-management/comb-traces.py
+++ kg-python/file-management/comb-traces.py
@@ -4,13 +4,14 @@ import os
 '''
 Search for a string in all files in a folder
 '''
-path = r"C:\Path\To\Trace\Folder"
-search_string = "INSERT INTO telco.GPS_POINT_TRANSIT"
+# path = r"C:\Path\To\Trace\Folder"
+path = r"C:\temp\intercepts"
+search_string = "select count(*) from sqlite_master"
 num = 0
 os.chdir(path)
-for files in glob.glob( "*.trc" ):
-    f = open( files, 'r' )
-    for line in f:
-        if search_string in line:
-            print("\t" + str(num) + ". " + f.name + " - " + line )
-            num = num + 1
+for _file in glob.glob( "*.362" ):
+    with open(_file, "r") as f:
+        for line in f:
+            if search_string in line:
+                print("\t" + str(num) + ". " + f.name + " - " + line )
+                num = num + 1
